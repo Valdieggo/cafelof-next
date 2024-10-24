d59b80e1473fbcf6baa3312c3e7b41046c03c209
Valdieggo <valdiego@mmac.local>
2024-10-24 17:49:01 -0300

    Error handler categorie product

--- src/app/api/product/categorie/create/route.ts
+++ src/app/api/product/categorie/create/route.ts
@@ -1,6 +1,6 @@
-const { PrismaClient } = require('@prisma/client')
+import { PrismaClient, Prisma } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
     const body = await request.json();
@@ -17,20 +17,46 @@ export async function POST(request: Request) {
         });
     }
 
-    const productCategorie = await prisma.productCategorie.create({
-        data: {
-            product_categorie_name,
-            product_categorie_description,
-        },
-    });
+    try {
+        const productCategorie = await prisma.productCategorie.create({
+            data: {
+                product_categorie_name,
+                product_categorie_description,
+            },
+        });
 
-    return new Response(JSON.stringify({
-        status: 201,
-        data: productCategorie,
-    }), {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+        console.log("productCategorie: ", productCategorie);
+        return new Response(JSON.stringify({
+            status: 201,
+            message: 'Categoría creada con exito',
+            data: productCategorie,
+        }), {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2002') {
+                return new Response(JSON.stringify({
+                    status: 400,
+                    message: 'La categoria del producto ya existe',
+                }), {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+            }
+        }
+
+        return new Response(JSON.stringify({
+            status: 400,
+            message: 'Error al crear categoria de producto',
+        }), {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
 }
