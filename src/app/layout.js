106852d036d4fa60e5980cdd9b99b98ba5b96fb9
Valdieggo <valdiego@mmac.local>
2024-10-29 21:13:37 -0300

    favicon metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,29 @@ import Footer from '../components/layout/Footer';
 export const metadata = {
   title: 'Café Lof',
   description: 'Café de alta calidad, tostado artesanalmente y personalizado a tu gusto, frescura garantizada. Solicítalo aquí!',
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/x-icon' },
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/x-icon' },
+    ],
+    apple: [
+      { url: '/favicon/apple-touch-icon.png', sizes: '180x180' },
+    ],
+    other: [
+      {
+        rel: 'mask-icon',
+        url: '/favicon/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+    ],
+  },
+  manifest: '/favicon/site.webmanifest',
+  meta: [
+    {
+      name: 'msapplication-TileColor',
+      content: '#da532c',
+    },
+  ],
 };
 
 export default function RootLayout({ children }) {
