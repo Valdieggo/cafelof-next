691a2f7c67716128ce9b036271b2cddd98cd71d0
Valdieggo <valdiego@mmac.local>
2024-11-11 17:08:54 -0300

    middleware and auth config

--- auth.ts
+++ auth.ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
 import { prisma } from "@/lib/prisma"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import authConfig from "./auth.config"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
-    providers: [GitHub, Google],
+    session: { strategy: "jwt" },
+    ...authConfig,
 })
--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from './auth'
-
-export default auth((req) => {
-    console.log("ROUTE: ", req.nextUrl.pathname)
-})
-
-export const config = {
-    matcher: ["'/((?!.\.\[\w\]+$_next).*)', '/', '/(api|trpc)(.*)'"],
-}
