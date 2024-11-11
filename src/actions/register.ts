55d6e8f167eb7a380236eb99aae33a97e8379ff0
Valdieggo <valdiego@mmac.local>
2024-11-11 14:12:10 -0300

    bcrypt added

---
+++ src/actions/register.ts
@@ -0,0 +1,35 @@
+"use server"
+import * as z from 'zod';
+import { RegisterSchema } from '../../schemas';
+import bcrypt from 'bcrypt';
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+    const validatedFields = RegisterSchema.safeParse(values);
+
+    if (!validatedFields.success) {
+        return { error: "Invalid fields" };
+    }
+
+    const requestValues = {
+        user_firstname: values.name,
+        user_lastname: values.lastname,
+        user_email: values.email,
+        user_password: values.password,
+    };
+
+    requestValues.user_password = await bcrypt.hash(requestValues.user_password, 10);
+
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+    const response = await fetch(`${baseUrl}/auth/user/create`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestValues),
+    });
+
+    const data = await response.json();
+    
+    return data;
+}
