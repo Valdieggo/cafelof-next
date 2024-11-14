54ae6cc632133f0c6af6f401b67cf4b0e1e7bd08
Valdieggo <valdiego@mmac.local>
2024-11-14 05:04:29 -0300

    api routes

--- src/app/api/country/route.ts
+++ src/app/api/country/route.ts
@@ -4,6 +4,16 @@ const prisma = new PrismaClient()
 
 export async function GET(){
     const allCountries = await prisma.country.findMany();
+    if(allCountries.length === 0){
+        return new Response(JSON.stringify({
+            status: 404,
+            message: 'No country found',
+        }), {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
 
     return new Response(JSON.stringify({
         status: 200,
