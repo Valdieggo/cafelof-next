0eb2cfb4c410e502cb1c538141656739e622c34f
Valdieggo <valdiego@mmac.local>
2024-11-14 05:02:26 -0300

    credentials, session and token

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,27 @@
 import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "./schemas";
+import bcrypt from "bcryptjs"
 
 import type { NextAuthConfig } from "next-auth";
 
 export default {
-    providers: [Google],
+    providers: [Google, Credentials({
+        async authorize(credentials){
+            const validatedFields = LoginSchema.safeParse(credentials);
+            if(validatedFields.success){
+                const { email, password } = validatedFields.data;
+
+                const user = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/user/${email}`).then(res => res.json());
+            
+                if(!user || !user.user_password) return null;
+
+                const passwordsMatch = await bcrypt.compare(password, user.user_password);
+
+                if(passwordsMatch) return user;
+
+                return null;
+            }
+        }
+    })],
 } satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -1,10 +1,51 @@
-import NextAuth from "next-auth"
-import { prisma } from "@/lib/prisma"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import authConfig from "./auth.config"
+import NextAuth, { type DefaultSession } from "next-auth";
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import authConfig from "./auth.config";
+
+declare module "next-auth" {
+  interface User {
+    user_email: string;
+    user_firstname: string;
+    user_lastname: string;
+    user_role: "USER" | "ADMIN";
+  }
+
+  interface Session {
+    user: {
+      role: "USER" | "ADMIN";
+    } & DefaultSession["user"];
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    session: { strategy: "jwt" },
-    ...authConfig,
-})
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.user_email;
+        token.name = `${user.user_firstname} ${user.user_lastname}`;
+        token.role = user.user_role;
+      }
+      console.log({ token, user });
+      return token;
+    },
+    async session({ session, token }) {
+      if (token.role && session.user) {
+        session.user = {
+          ...session.user,
+          id: token.id as string,
+          email: token.email as string,
+          name: token.name as string,
+          role: token.role as "USER" | "ADMIN",
+        };
+      }
+      console.log({ token, user: session.user });
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  debug: true,
+  ...authConfig,
+});
