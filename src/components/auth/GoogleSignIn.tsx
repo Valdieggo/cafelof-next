e174a177e03fa08067ebf5d97ddc115ef8673a72
Valdieggo <valdiego@mmac.local>
2024-11-15 04:38:50 -0300

    signIn callback modified

--- src/components/auth/GoogleSignIn.tsx
+++ src/components/auth/GoogleSignIn.tsx
@@ -2,14 +2,11 @@
 import { signIn } from "next-auth/react"; // Import from next-auth/react
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
+import { DEFAULT_ADMIN_LOGIN_REDIRECT } from "../../../routes";
 
 export default function GoogleSignIn() {
-  const handleSignIn = async () => {
-    try {
-      await signIn("google"); // Client-side sign-in
-    } catch (error) {
-      console.error("Error signing in: ", error);
-    }
+  const handleSignIn = () => {
+    signIn("google", { callbackUrl: DEFAULT_ADMIN_LOGIN_REDIRECT });
   };
 
   return (
