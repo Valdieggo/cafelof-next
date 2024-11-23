a15067e8dde2d71dbc6f2fd822a3dcd5ef2cd83d
Valdieggo <valdiego@mmac.local>
2024-11-23 03:44:02 -0300

    server action deleted

--- src/app/productos/page.js
+++ src/app/productos/page.js
@@ -1,14 +1,18 @@
-import { getProducts } from '@/actions/getProducts';
 import Products from "@/components/products/Products";
 
 export default async function Page() {
-  const products = await getProducts();
+  const data = await fetch('http://localhost:3000/api/product', {
+    next: { revalidate: 10 }, // Opcional: para revalidar caché cada 10 segundos
+  });
+
+  let products = await data.json();
+
   const categories = ['Café de grano', 'Accesorios'];
   const sortOptions = ['Orden predeterminado', 'Precio ascendente', 'Precio descendente'];
 
   return (
     <Products 
-      initialProducts={products} 
+      initialProducts={products.data} 
       categories={categories} 
       sortOptions={sortOptions} 
     />
