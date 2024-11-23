fffa4b2599a94cda4d1f5155e44660b9229bbc31
Valdieggo <valdiego@mmac.local>
2024-11-23 04:21:39 -0300

    server action deleted

--- src/app/(protected)/profile/page.js
+++ src/app/(protected)/profile/page.js
@@ -1,12 +1,12 @@
 import UserProfile from "@/components/profile/UserProfile";
 import { auth, signOut } from "../../../../auth";
-import { getUser } from "@/actions/getUser";
-import { Button } from "@/components/ui/button";
 import SignOut from "@/components/auth/signOut";
 
 export default async function Page(){
     const session = await auth();
-    const user = await getUser(session.user.id);
+    const data = await fetch(`http://localhost:3000/api/auth/user/${session.user.id}`);
+
+    let user = await data.json();
 
     return (
         <div className="w-full max-w-md mx-auto mt-8 mb-8">
