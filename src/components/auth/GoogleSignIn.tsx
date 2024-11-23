31d9475d1a3df4644d92317f39b223ee9b704157
Valdieggo <valdiego@mmac.local>
2024-11-23 01:40:53 -0300

    redirect route changed

--- src/components/auth/GoogleSignIn.tsx
+++ src/components/auth/GoogleSignIn.tsx
@@ -2,11 +2,11 @@
 import { signIn } from "next-auth/react"; // Import from next-auth/react
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
-import { DEFAULT_ADMIN_LOGIN_REDIRECT } from "../../../routes";
+import { DEFAULT_LOGIN_REDIRECT } from "../../../routes";
 
 export default function GoogleSignIn() {
   const handleSignIn = () => {
-    signIn("google", { callbackUrl: DEFAULT_ADMIN_LOGIN_REDIRECT });
+    signIn("google", { callbackUrl: DEFAULT_LOGIN_REDIRECT });
   };
 
   return (
