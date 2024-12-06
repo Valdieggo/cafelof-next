d5a6dd0f8f8fc8a4c019a9b36d55805148f0f546
Valdieggo <diego.valdera98@gmail.com>
2024-12-06 05:33:13 -0300

    comment

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import PhotoCarousel from "../components/layout/Carousel";
 import ProductCategories from "../components/products/ProductCategories";
 import Hero from "@/components/Hero"
 
+//Café Lof
 export default async function Page() {
   return (
     <div>
