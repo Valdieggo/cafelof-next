172e52b0b508a731b50c0462057f9c22de7b9c27
Valdieggo <diego.valdera98@gmail.com>
2024-12-08 18:59:56 -0300

    comments deleted

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,19 +22,10 @@ export async function middleware(req: any) {
         return NextResponse.next();
     }
 
-    if(isLoggedIn){
-      console.log("User is logged in !!!!");
-    }
-
     if (isAuthRoute) {
-        console.log("Api route");
         if (isLoggedIn) {
-          console.log("User is logged in");
             return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, req.nextUrl));
-        }else {
-          console.log("User is not logged in");
         }
-        console.log("req: ", req, "token: ", token);
         return NextResponse.next();
     }
 
