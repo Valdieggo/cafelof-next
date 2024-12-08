db5c801f3fae9cf3bcb6650e30c32401c5622b95
Valdieggo <diego.valdera98@gmail.com>
2024-12-08 19:32:33 -0300

    deleted comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,44 +3,35 @@ import { NextResponse } from "next/server";
 import { apiPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, adminRoutePrefix } from "../routes";
 
 export async function middleware(req: any) {
-    const { pathname } = req.nextUrl;
+  const { pathname } = req.nextUrl;
 
-    const isApiRoute = pathname.startsWith(apiPrefix);
-    const isAuthRoute = authRoutes.includes(pathname);
-    const isAdminRoute = pathname.startsWith(adminRoutePrefix);
+  const isApiRoute = pathname.startsWith(apiPrefix);
+  const isAuthRoute = authRoutes.includes(pathname);
+  const isAdminRoute = pathname.startsWith(adminRoutePrefix);
 
-    const token = await getToken({ req, secret: process.env.AUTH_SECRET, secureCookie: true });
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET, secureCookie: true });
 
-    const isLoggedIn = !!token;
-    const userRole = token?.role;
+  const isLoggedIn = !!token;
+  const userRole = token?.role;
 
-    if (isAdminRoute && userRole !== "ADMIN") {
-        return NextResponse.redirect(new URL("/", req.nextUrl));
-    }
+  if (isAdminRoute && userRole !== "ADMIN") {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
 
-    if (isApiRoute) {
-        return NextResponse.next();
-    }
-
-    if(isLoggedIn){
-      console.log("User is logged in !!!!");
-    }
-
-    if (isAuthRoute) {
-        console.log("Api route");
-        if (isLoggedIn) {
-          console.log("User is logged in");
-            return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, req.nextUrl));
-        }else {
-          console.log("User is not logged in");
-        }
-        console.log("req: ", req, "token: ", token);
-        return NextResponse.next();
-    }
-
+  if (isApiRoute) {
     return NextResponse.next();
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, req.nextUrl));
+    }
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
