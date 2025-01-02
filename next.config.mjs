3f0be86768d650fc21a18a36de33d7608cd584c5
Valdieggo <diego.valdera98@gmail.com>
2025-01-02 17:33:19 -0300

    security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-    images: {
-        remotePatterns: [
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.dog.ceo",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // Aplica los encabezados a todas las rutas
+        source: "/(.*)",
+        headers: [
           {
-            protocol: "https",
-            hostname: "images.dog.ceo",
-            pathname: "**",
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin", // Protección contra envío de referencias
           },
           {
-            protocol: "https",
-            hostname: "lh3.googleusercontent.com",
-            pathname: "**",
-          }
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN", // Permite mostrar contenido en iframes solo desde el mismo origen
+          },
         ],
       },
+    ];
+  },
 };
 
 export default nextConfig;
