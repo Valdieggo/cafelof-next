aa25eede5e60a7d863ae5debb37b7db0aaddf583
Valdieggo <diego.valdera98@gmail.com>
2025-01-03 01:52:32 -0300

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -18,38 +18,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // Aplica los encabezados a todas las rutas principales
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' https://apis.google.com;
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' https://images.dog.ceo https://lh3.googleusercontent.com;
-              font-src 'self';
-              connect-src 'self';
-              frame-src 'self' https://apis.google.com;
-            `.replace(/\s{2,}/g, " ").trim(),
-          },
-        ],
-      },
-      {
-        // Aplica los encabezados a recursos estáticos en _next/static
-        source: "/_next/static/(.*)",
+        source: "/(.*)", // Apply to all routes, including _next/static
         headers: [
           {
             key: "Referrer-Policy",
